c368578538b811484013c56144c46ae241ce5b52
Maykon Morais <maykon.estudos@gmail.com>
2021-11-01 22:57:42 -0300

    feat(modality): add modality details

---
+++ src/pages/modality/[name].tsx
@@ -0,0 +1,11 @@
+import Header from '@components/common/Header'
+import ModalityDetail from '@components/modality/ModalityDetail'
+
+export default function Modality() {
+	return (
+		<>
+			<Header />
+			<ModalityDetail />
+		</>
+	)
+}
