824b2107575ca41a74f61a8496abf7374dc6905f
Renan Ribeiro <Renanribeirow19@gmail.com>
2021-11-05 16:51:18 -0300

    feature : list items

---
+++ src/components/Schendule/Tittle.tsx
@@ -0,0 +1,11 @@
+import { Container } from './styles'
+
+export default function Head() {
+	return (
+		<Container>
+			<span>De hoje até 22 de março</span>
+			<span>Você tem 1 aula disponível para remarcação.</span>
+			<button>Remarcar agora</button>
+		</Container>
+	)
+}
--- src/pages/Schedule.tsx
+++ src/pages/Schedule.tsx
@@ -1,19 +1,38 @@
 import Navbar from '../components/Navbar/Navbar'
 import Item from '../components/Item/Item'
+import Tittle from '@components/Schendule/Tittle'
 import { ModelItem } from '@src/types'
+
 const LandingPage = () => {
-	const i: ModelItem = {
-		title: 'asda',
-		num: 25,
-		subtitle: 'fsdfs',
-		locale: 'sdfs',
-	}
+	const data: Array<ModelItem> = [
+		{
+			title: 'Judô',
+			num: 24,
+			subtitle: '14:20 - 13:20',
+			locale: 'Sede Principal',
+		},
+		{
+			title: 'Iôga',
+			num: 29,
+			subtitle: '11:00 - 12:00',
+			locale: 'Sede Principal',
+		},
+		{
+			title: 'Judô',
+			num: 22,
+			subtitle: '17:30 - 18:30',
+			locale: 'Sede Principal',
+		},
+	]
 
 	return (
 		<div>
 			<Navbar />
-
-			<Item item={i} haveLocale={true} haveTrash={true} />
+			<Tittle />
+			{data.map(function (item) {
+				// eslint-disable-next-line react/jsx-key
+				return <Item item={item} haveLocale={true} haveTrash={true} />
+			})}
 		</div>
 	)
 }
