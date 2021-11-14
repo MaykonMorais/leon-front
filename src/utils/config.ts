fe7633b51697855b6a2e5f9eb40e4a85764f9b32
Maykon Morais <maykon.estudos@gmail.com>
2021-11-14 12:00:03 -0300

    feat: add utils config

---
+++ src/utils/config.ts
@@ -0,0 +1,12 @@
+export const config = {
+	API_URL: process.env.API_URL,
+
+	tokenType: 'Bearer',
+
+	storageUser: '@LEON:User',
+	storageUserToken: '@LEON:Token',
+
+	endpoints: {
+		users: '/users',
+	},
+}
