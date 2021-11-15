9b588d902b9bf6d1e758798adbbd854dc377e99f
Maykon Morais <maykon.estudos@gmail.com>
2021-11-15 09:38:00 -0300

    feat: add validation to login

--- src/components/login/LoginBox/index.tsx
+++ src/components/login/LoginBox/index.tsx
@@ -1,6 +1,10 @@
 import Link from 'next/link'
 import { FiArrowRight } from 'react-icons/fi'
 
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+
 import {
 	Container,
 	Card,
@@ -20,8 +24,27 @@ import {
 	RegisterArea,
 	RegisterButton,
 } from './styles'
+import InputError from '@src/components/common/InputError'
+
+const loginSchema = yup.object({
+	email: yup
+		.string()
+		.email('Insira um e-mail válido')
+		.required('Campo Obrigatório'),
+	password: yup.string().required('Campo obrigatório'),
+})
 
 export default function Box() {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		resolver: yupResolver(loginSchema),
+	})
+
+	const onSubmit = data => console.log(data)
+
 	return (
 		<Container>
 			<Card>
@@ -36,18 +59,25 @@ export default function Box() {
 							<Logo src='/Logo.png' />
 						</RightHeader>
 					</Header>
+					<form onSubmit={handleSubmit(onSubmit)}>
+						<TopContent>
+							<LoginText>Por favor, preencha os campos abaixo</LoginText>
 
-					<TopContent>
-						<LoginText>Por favor, preencha os campos abaixo</LoginText>
+							<Label htmlFor='email'>E-mail</Label>
+							<Input {...register('email')} id='email' type='email' />
+							{errors.email?.message && (
+								<InputError error={errors.email?.message} />
+							)}
 
-						<Label htmlFor='email'>E-mail</Label>
-						<Input id='email' type='email' />
+							<Label htmlFor='password'>Senha</Label>
+							<Input {...register('password')} id='password' type='password' />
+							{errors.password?.message && (
+								<InputError error={errors.password?.message} />
+							)}
 
-						<Label htmlFor='password'>Senha</Label>
-						<Input id='password' type='password' />
-
-						<SignInButton>Entrar</SignInButton>
-					</TopContent>
+							<SignInButton type='submit'>Entrar</SignInButton>
+						</TopContent>
+					</form>
 
 					<BottomContent>
 						<RegisterArea>
