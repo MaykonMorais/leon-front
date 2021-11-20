ae5c1662ce6892b2c00f6e468e899f2c5c0df6c5
Maykon Morais <maykon.estudos@gmail.com>
2021-11-20 11:07:49 -0300

    chore: add token and redux state and action types

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,23 @@ export interface ILogin {
 	email: string
 	password: string
 }
+
+export interface IToken {
+	sub: string
+	roles: [string]
+	iss: string
+	exp: number
+}
+
+export interface IAction {
+	type: string
+}
+
+export interface IUserState {
+	status: string
+	authenticated: boolean
+}
+
+export interface IRootState {
+	user: IUserState
+}
