281016ac15685d8b7909fc59c79b514be9df53f3
Maykon Morais <maykon.estudos@gmail.com>
2021-11-20 12:23:35 -0300

    feat: add toast on 403 error

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,24 +1,41 @@
+import { toast } from 'react-toastify'
 import { config as defaultConfigs } from '@utils/config'
+import { parseCookies } from 'nookies'
 import axios from 'axios'
 
 export const baseURL = defaultConfigs.API_URL
 
 const axiosConfig = axios.create({
-	baseURL,
+	baseURL: baseURL,
 })
 
-axios.interceptors.request.use(
+axiosConfig.interceptors.request.use(
 	config => {
-		const userToken = localStorage.getItem(defaultConfigs.storageUserToken)
+		const { [`${defaultConfigs.storageUserToken}`]: token } = parseCookies()
 
-		if (userToken) {
+		if (token) {
 			// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-			config.headers!.Authorization = `${defaultConfigs.tokenType} ${userToken}`
+			config.headers!.Authorization = `${defaultConfigs.tokenType} ${token}`
 		}
 
 		return config
 	},
 	error => Promise.reject(error)
+)
+
+axiosConfig.interceptors.response.use(
+	response => {
+		return response
+	},
+	error => {
+		if (error.response.status === 403) {
+			const notify = () => toast.error('Ops! Usuário ou senha incorretos')
+
+			notify()
+		}
+
+		return Promise.reject(error)
+	}
 )
 
 export default axiosConfig
