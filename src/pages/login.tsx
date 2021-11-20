d199751496acbb0e9049e87407c479bdeef84af7
Maykon Morais <maykon.estudos@gmail.com>
2021-11-20 11:10:50 -0300

    feat: add server side token verification on login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,3 +1,8 @@
+import { GetServerSideProps } from 'next'
+
+import { config } from '@src/utils/config'
+import { parseCookies } from 'nookies'
+
 import LoginBox from '@src/components/login/LoginBox'
 
 const Login = () => {
@@ -6,6 +11,23 @@ const Login = () => {
 			<LoginBox />
 		</>
 	)
+}
+
+export const getServerSideProps: GetServerSideProps = async ctx => {
+	const { [`${config.storageUserToken}`]: token } = parseCookies(ctx)
+
+	if (token) {
+		return {
+			redirect: {
+				destination: '/admin-home',
+				permanent: false,
+			},
+		}
+	}
+
+	return {
+		props: {},
+	}
 }
 
 export default Login
