09fd0ff345b180cd3ca7854a2798d42409cfc3dc
Maykon Morais <maykon.estudos@gmail.com>
2021-11-21 20:06:39 -0300

    feat: add search modality service

--- src/services/modalities.ts
+++ src/services/modalities.ts
@@ -6,3 +6,9 @@ const { modalities } = config.endpoints
 export async function getModalitiesData() {
 	return await api.get(modalities).then(response => response.data)
 }
+
+export async function searchModalityByName(name: string) {
+	return await api
+		.get(`${modalities}/search?name=${name}`)
+		.then(response => response.data)
+}
