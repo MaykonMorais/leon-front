3ccf679d85bc52a8b81e0239772c10fa1bf5d58d
Maykon Morais <maykon.estudos@gmail.com>
2021-11-21 11:16:13 -0300

    feat: add month title

---
+++ src/components/schedule/MonthScheduled/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Month = styled.h2`
+	font-size: 22px;
+	font-weight: 600;
+	line-height: 29px;
+
+	color: rgba(0, 0, 0, 0.8);
+
+	margin-bottom: 8px;
+`
