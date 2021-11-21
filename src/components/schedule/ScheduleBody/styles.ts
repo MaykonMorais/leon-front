2abe14aca41442d90363e9c2759ad7c8f46bf898
Maykon Morais <maykon.estudos@gmail.com>
2021-11-20 23:22:07 -0300

    feat: add responsive layout on schedule body

--- src/components/schedule/ScheduleBody/styles.ts
+++ src/components/schedule/ScheduleBody/styles.ts
@@ -1,5 +1,10 @@
 import styled from 'styled-components'
+import media from 'styled-media-query'
 
 export const Container = styled.div`
-	padding: 40px;
+	padding: 40px 90px;
+
+	${media.lessThan('medium')`
+      padding: 20px 10px;
+  `}
 `
