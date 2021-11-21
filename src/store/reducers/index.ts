08516d8196b60995e91f2ff8bce6d982a813955a
Maykon Morais <maykon.estudos@gmail.com>
2021-11-21 18:44:36 -0300

    feat: add modalities reducer to globals reducers

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -3,7 +3,8 @@ import { combineReducers } from 'redux'
 // reducers
 import user from './user'
 import schedule from './schedule'
+import modalities from './modalities'
 
-const rootReducer = combineReducers({ user, schedule })
+const rootReducer = combineReducers({ user, schedule, modalities })
 
 export default rootReducer
