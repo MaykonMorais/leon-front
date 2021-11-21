548519abb04021091b47cf5aefe9c740b5bbbf34
Maykon Morais <maykon.estudos@gmail.com>
2021-11-20 23:21:43 -0300

    feat: add responsive layout on schedule head

--- src/components/schedule/Head/styles.ts
+++ src/components/schedule/Head/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import media from 'styled-media-query'
 
 interface InfoMarkdownProps {
 	bold?: boolean
@@ -8,7 +9,12 @@ export const Container = styled.div`
 	display: flex;
 	justify-content: space-between;
 
+	${media.lessThan('medium')`
+    flex-direction: column;
+  `}
+
 	padding: 4px 0px;
+	margin-bottom: 16px;
 `
 
 export const LeftContent = styled.div``
@@ -22,11 +28,25 @@ export const DayText = styled.p`
 	font-weight: 500;
 	font-size: 24px;
 	line-height: 29px;
+
+	${media.lessThan('medium')`
+    margin-bottom: 8px;
+
+    font-weight: 500;
+    font-size: 22px;
+    line-height: 29px;
+  `}
 `
 
 export const InfoMarkdown = styled.span<InfoMarkdownProps>`
 	font-size: 15px;
-	line-height: 24px;
+	line-height: 20px;
+
+	${media.lessThan('medium')`
+      width: 100%;
+      font-size: 14px;
+      line-height: 24px;
+  `}
 
 	font-weight: ${({ bold }) => (bold ? '700' : '400')};
 `
@@ -37,6 +57,10 @@ export const RescheduleButton = styled.button`
 	padding: 4px;
 
 	font-size: 16px;
+
+	${media.lessThan('medium')`
+      font-size: 14px;
+  `}
 
 	margin-top: 4px;
 `
