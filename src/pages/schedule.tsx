19eab2ce31659aaf4107891d8a838eb339619620
Maykon Morais <maykon.estudos@gmail.com>
2021-11-21 11:22:38 -0300

    refactor: refact schedule page

--- src/pages/schedule.tsx
+++ src/pages/schedule.tsx
@@ -1,30 +1,47 @@
 import Header from '@components/common/Header'
 import BodyContainer from '@components/common/BodyContainer'
-import { ModelItem } from '@types'
+import { ModelItem, MonthScheduled } from '@types'
 
 import HeadSchedule from '@components/schedule/Head'
 import ScheduleBody from '@components/schedule/ScheduleBody'
 import ScheduleCard from '@components/schedule/ScheduleCard'
+import MonthTitle from '@components/schedule/MonthScheduled'
 
-const LandingPage = () => {
-	const data: Array<ModelItem> = [
+const Schedule = () => {
+	const months: Array<MonthScheduled> = [
 		{
-			title: 'Judô',
-			num: 24,
-			subtitle: '14:20 - 13:20',
-			locale: 'Sede Principal',
+			month: 'Fevereiro',
+			days: [
+				{
+					title: 'Judô',
+					num: 24,
+					subtitle: '14:20 - 13:20',
+					locale: 'Sede Principal',
+				},
+				{
+					title: 'Iôga',
+					num: 29,
+					subtitle: '11:00 - 12:00',
+					locale: 'Sede Principal',
+				},
+			],
 		},
 		{
-			title: 'Iôga',
-			num: 29,
-			subtitle: '11:00 - 12:00',
-			locale: 'Sede Principal',
-		},
-		{
-			title: 'Judô',
-			num: 22,
-			subtitle: '17:30 - 18:30',
-			locale: 'Sede Principal',
+			month: 'Março',
+			days: [
+				{
+					title: 'Judô',
+					num: 24,
+					subtitle: '14:20 - 13:20',
+					locale: 'Sede Principal',
+				},
+				{
+					title: 'Iôga',
+					num: 29,
+					subtitle: '11:00 - 12:00',
+					locale: 'Sede Principal',
+				},
+			],
 		},
 	]
 
@@ -35,14 +52,23 @@ const LandingPage = () => {
 				<ScheduleBody>
 					<HeadSchedule />
 
-					{data.map((item, index) => {
+					{months.map(({ month, days }, index) => {
 						return (
-							<ScheduleCard
-								key={index}
-								item={item}
-								haveLocale={true}
-								haveTrash={true}
-							/>
+							<>
+								<MonthTitle key={index} month={month} />
+
+								{days.map((day, index) => {
+									return (
+										<ScheduleCard
+											duration={0.7 + 0.4 * index}
+											key={index}
+											item={day}
+											haveLocale
+											haveTrash
+										/>
+									)
+								})}
+							</>
 						)
 					})}
 				</ScheduleBody>
@@ -51,4 +77,4 @@ const LandingPage = () => {
 	)
 }
 
-export default LandingPage
+export default Schedule
